3cc1e2e3378289c5260c8bd9283062a4574c10f6
Joey Aviles <pleasantmkgworld@gmail.com>
2020-07-11 11:32:58 -0400

    Updated code style with autopep8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lambda-DSPT6_javiles", # Replace with your own username
+    name="lambda-DSPT6_javiles",  # Replace with your own username
     version="0.0.1",
     author="Joey Aviles",
     author_email="author@example.com",
